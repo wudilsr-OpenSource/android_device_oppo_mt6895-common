73e969d794686adeddddc62ea04396d5f1f2b97d
Nolen Johnson <johnsonnolen@gmail.com>
2025-05-17 16:52:40 +0800

    mt6895-common: Uprev keymint dependency from V3-ndk to V4-ndk
    * QPR2 doesn't let us link V3-ndk anymore.
    
    Change-Id: Iac86624a207b19d89af0932768d62e1e932eb798

--- extract-files.py
+++ extract-files.py
@@ -64,7 +64,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     'vendor/bin/hw/android.hardware.security.keymint-service.trustonic': blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so')
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V4-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so'),
     (
