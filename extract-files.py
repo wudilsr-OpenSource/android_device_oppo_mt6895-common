4984d3b4438e9d3b0eae0e78ec8b694bfb5e9353
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-05-17 09:07:57 +0800

    garnet: Address missing SetTaskProfiles symbol in camera libs
    Could not load /vendor/lib64/hw/hwcomposer.mtk_common.so from default namespace: dlopen failed: cannot locate symbol "SetTaskProfiles" referenced by "/vendor/lib64/hw/hwcomposer.mtk_common.so"....
    
    wudilsr@pop-os:~/android/crdroid15/vendor/oppo/mt6895-common$  grep -i -r SetTaskProfiles
    grep: proprietary/vendor/lib64/mt6895/libcam.hal3a.ctrl.so: 匹配到二进制文件
    grep: proprietary/vendor/lib64/mt6895/libcam.hal3a.so: 匹配到二进制文件
    grep: proprietary/vendor/lib64/mt6895/libmtkcam_request_requlator.so: 匹配到二进制文件
    grep: proprietary/vendor/lib64/hw/hwcomposer.mtk_common.so: 匹配到二进制文件
    grep: proprietary/vendor/lib64/libcustomer_cameradata.so: 匹配到二进制文件
    
    This is because DECLS was removed in [1] on Android 15 QPR2, and
    caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles
    with libprocessgroup_shim.
    
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Change-Id: Ic46aae47bdf6be09736fe89d798cb7017f07cc6c
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,14 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libshim_sensors.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
+    (
+        'vendor/lib64/hw/hwcomposer.mtk_common.so',
+        'vendor/lib64/mt6895/libcam.hal3a.so',
+        'vendor/lib64/mt6895/libcam.hal3a.ctrl.so',
+        'vendor/lib64/mt6895/libmtkcam_request_requlator.so',
+        'vendor/lib64/libcustomer_cameradata.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/hw/mt6895/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
