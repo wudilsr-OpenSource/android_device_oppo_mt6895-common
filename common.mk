9cc35065cb6d69ca9aa6c919d394f8303e928473
Guidix <guidixm@gmail.com>
2025-05-17 09:31:59 +0800

    mt6895-common: Build android.hardware.audio.sounddose-vendor-impl.so
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    W audiohalservice: createISoundDoseFactory() from android.hardware.audio.sounddose-vendor-impl failed

--- common.mk
+++ common.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
