a185d76cdd45ee154dfebd14d1db491d45560e35
wudilsr <wudilsr@gmail.com>
2025-05-18 20:50:52 +0800

    mt6895-common: media: Add dummy mtk_platform_codecs_whitelist.xml
    05-18 08:38:13.651 D/C2MtkVdec( 1758): [0xB40000732E70C918] Failed to open: /vendor/etc/mtk_platform_codecs_whitelist.xml
    05-18 08:38:13.651 D/C2MtkVdecExt( 1758): [0xB40000732E70C918] Cannot create whitelist from /vendor/etc/mtk_platform_codecs_whitelist.xml

--- common.mk
+++ common.mk
@@ -215,6 +215,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/media/mtk_platform_codecs_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_platform_codecs_whitelist.xml
+
+
 # Net
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
